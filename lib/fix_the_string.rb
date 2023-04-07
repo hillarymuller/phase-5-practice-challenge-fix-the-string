876e483dbd89c34eefc79822bf46cb29ccbee214
Hillary Muller <hillaryaward@gmail.com>
2023-04-07 14:10:30 -0500

    add solution

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,1 +1,17 @@
-# your code here
+def fix_the_string(str)
+    #establish a stack 
+    stack = []
+    #iterate over each character in the string
+    str.each_char do |c|
+        # if the stack is not empty and the character doesn't exactly equal the 
+        # last item in the stack without changing case & the character upcased does equal the last stack
+        # item upcased remove the last item from the stack and move on to next character
+        if !stack.empty? && c != stack.last && c.upcase == stack.last.upcase
+            stack.pop
+            #otherwise add the character to the top of the stack
+        else stack.push(c)
+        end
+    end
+    # join the stack array into a string & return
+    return stack.join()
+end
