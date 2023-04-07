93e2be570c01ef82b1d19b9cec5e47e1c0ea4d39
Hillary Muller <hillaryaward@gmail.com>
2023-04-07 14:05:32 -0500

    commit tree ff8b6d9ca66e74a6bfa5713b0fa1b081dd10eef4

--- lib/fix_the_string.rb
+++ lib/fix_the_string.rb
@@ -1,1 +1,10 @@
-# your code here
+def fix_the_string(str)
+    stack = []
+    str.each_char do |c|
+        if !stack.empty? && c != stack.last && c.upcase == stack.last.upcase
+            stack.pop
+        else stack.push(c)
+        end
+    end
+    return stack.join()
+end
